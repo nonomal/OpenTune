bbf264b493238c9913f4ed40e8ebcbe99cc301f7
Arturo Cervantes <artuscervantes111@gmail.com>
2025-08-10 18:46:33 -0600

    Chore: Update app version to 2.0.4
    This commit increments the `versionCode` to 118 and updates the `versionName` to "2.0.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.Arturo254.opentune"
         minSdk = 24
         targetSdk = 35
-        versionCode = 117
-        versionName = "2.0.3"
+        versionCode = 118
+        versionName = "2.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
